d7ffda4d31d8f5d64bad261d3655bc00b2b552b7
Joey Schuringa <jschuringa86@gmail.com>
2019-04-06 13:13:26 -0500

    feat(Calculator): add initial module and tests

---
+++ weapon/tests/tests.py
@@ -0,0 +1,35 @@
+import unittest
+from weapon import weapon
+class TestCalculator(unittest.TestCase):
+
+    def test_type_does_more_damage(self):
+        axe_with_fire = weapon.Axe(10, 20, 5, "fire", 10)
+        axe_without_fire = weapon.Axe(10, 20, 5, "ice", 10)
+        aws = axe_with_fire.get_smite_damage(0)
+        awos = axe_without_fire.get_smite_damage(0)
+        self.assertGreater(aws, awos)
+
+    def test_provided_test_case(self):
+        """
+            Asserts through almost equal due to possible rounding differences
+        """
+        axe = weapon.Axe(50, 65, 20, "fire", 10)
+        damage = axe.get_damage_dict(10)
+        punch = damage["Punch"]
+        smite = damage["Smite"]
+        cleave = damage["Cleave"]
+
+        self.assertAlmostEqual(60, punch["Minimum"], delta=2)
+        self.assertAlmostEqual(78, punch["Maximum"], delta=2)
+        self.assertAlmostEqual(82.8, punch["DPS"], delta=2)
+        
+        self.assertAlmostEqual(59.4, smite["Minimum"], delta=2)
+        self.assertAlmostEqual(77.22, smite["Maximum"], delta=2)
+        self.assertAlmostEqual(98.366, smite["DPS"], delta=2)
+
+        self.assertAlmostEqual(72, cleave["Minimum"], delta=2)
+        self.assertAlmostEqual(93.6, cleave["Maximum"], delta=2)
+        self.assertAlmostEqual(89.424, cleave["DPS"], delta=2)
+
+        if __name__ == '__main__':
+            unittest.main()
---
+++ weapon/weapon.py
@@ -0,0 +1,69 @@
+class Weapon():
+    aps = 1
+
+    def __init__(self, min_dmg, max_dmg, strength, ele_type, ele_dmg):
+        self.min_dmg = min_dmg
+        self.max_dmg = max_dmg
+        self.strength = strength
+        self.ele_type = ele_type
+        self.ele_mod = ele_dmg / 100
+    
+    def get_min_dmg(self, str_mod, use_ele_mod):
+        return self.min_dmg * (self.ele_mod + str_mod) if use_ele_mod else self.min_dmg * str_mod
+
+    def get_max_dmg(self, str_mod, use_ele_mod):
+        return self.max_dmg * (self.ele_mod + str_mod) if use_ele_mod else self.max_dmg * str_mod
+
+    def get_dps(self, min_dmg, max_dmg, aps):
+        return (min_dmg + max_dmg) / 2 * aps
+
+    def get_damage_dict(self, pc_lvl):
+        strength = (pc_lvl * 10 / 100) + (self.strength / 100)
+        damageDict = {}
+        damageDict["Punch"] = self.get_punch_damage(strength)
+        damageDict["Smite"] = self.get_smite_damage(strength)
+        damageDict["Cleave"] = self.get_cleave_damage(strength)
+        return damageDict
+
+    def get_punch_damage(self, str_mod):
+        use_ele_mod = self.ele_type == "phsyical"
+        atk_min_dmg = self.get_min_dmg(str_mod, use_ele_mod)
+        atk_max_dmg = self.get_max_dmg(str_mod, use_ele_mod)
+
+        return Weapon.build_atk_dict(atk_min_dmg, atk_max_dmg, self.get_dps(atk_min_dmg, atk_max_dmg, self.aps))
+
+    def get_smite_damage(self, str_mod):
+        use_ele_mod = self.ele_type == "fire"
+        atk_min_dmg = self.get_min_dmg(str_mod, use_ele_mod) * .9
+        atk_max_dmg = self.get_max_dmg(str_mod, use_ele_mod) * .9
+
+        return Weapon.build_atk_dict(atk_min_dmg, atk_max_dmg, self.get_dps(atk_min_dmg, atk_max_dmg, self.aps * 1.2))
+
+    def get_cleave_damage(self, str_mod):
+        use_ele_mod = self.ele_type == "ice"
+
+        atk_min_dmg = self.get_min_dmg(str_mod, use_ele_mod) * 1.2
+        atk_max_dmg = self.get_max_dmg(str_mod, use_ele_mod) * 1.2
+
+        return Weapon.build_atk_dict(atk_min_dmg, atk_max_dmg, self.get_dps(atk_min_dmg, atk_max_dmg, self.aps * .9))
+
+    @staticmethod
+    def build_atk_dict(min_dmg, max_dmg, dps):
+        atkDict = {}
+        atkDict["Minimum"] = min_dmg
+        atkDict["Maximum"] = max_dmg
+        atkDict["DPS"]  = dps
+        return atkDict
+
+class Mace(Weapon):
+    aps = 1
+
+class Axe(Weapon):
+    aps = 1.2
+
+class Sword(Weapon):
+    aps = 1.4
+
+axe = Axe(50, 65, 20, "fire", 10)
+print(axe.get_damage_dict(10))
+
